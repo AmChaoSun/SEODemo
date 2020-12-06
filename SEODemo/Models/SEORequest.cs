c248abdad4b538ac21dc9bfb5d49ad333288cf20
AmChaoSun <Julie1224!>
2020-12-06 20:14:05 +1100

    add comments

--- SEODemo/Models/SEORequest.cs
+++ SEODemo/Models/SEORequest.cs
@@ -7,8 +7,21 @@ namespace SEODemo.Models
 {
     public class SEORequest
     {
+        /// <summary>
+        /// This is the name of engine,
+        /// Currently only: "Google", "Bing", "InfoTrackGoogle" are allowed
+        /// </summary>
         public string Engine { get; set; }
+
+        /// <summary>
+        /// Search string
+        /// e.g. online title search
+        /// </summary>
         public string Input { get; set; }
+
+        /// <summary>
+        /// For what we are looking for in SEO
+        /// </summary>
         public string Target { get; set; }
     }
 }
